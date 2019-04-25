a0ede0e69cb3bf64d834c7421285551ef85d037b
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 11:43:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {Flour: '1 Gram', Eggs: 'Two', Milk: 'One Cup'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return recipes.assign(recipes,{key:value})
+  object[key] = value;
+  return object
 }
