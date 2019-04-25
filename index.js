ac4c5cb34c986ce52b41b6d75ac511c493be8a72
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 11:59:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {Flour: '1 Gram', Eggs: 'Two', Milk: 'One Cup'};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var cloneObject = Object.assign({}, object, {key:value});
+  var cloneObject = Object.assign({}, object, {[key]:value});
   return cloneObject
 }
 
